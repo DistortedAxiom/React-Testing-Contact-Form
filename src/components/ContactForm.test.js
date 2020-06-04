a5985db2c50821a0a807c3a5ee018f82ea68431d
Anh Truong <aqtruong97@gmail.com>
2020-06-04 15:51:26 -0400

    Finished MVP

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from "./ContactForm";
+import { act } from "react-dom/test-utils";
+
+test("Form rendered successfully", () => {
+  render(<ContactForm />);
+});
+
+test("make sure form is submitting", async () => {
+  const { getByLabelText, getByRole } = render(<ContactForm />);
+
+  const firstName = getByLabelText(/First Name*/i);
+  const lastName = getByLabelText(/Last Name*/i);
+  const email = getByLabelText(/Email/i);
+  const message = getByLabelText(/Message/i);
+  const button = getByRole('button');
+
+  fireEvent.change(firstName, { target: { value: "Anh" } });
+  fireEvent.change(lastName, { target: { value: "Truong" } });
+  fireEvent.change(email, { target: { value: "test@gmail.com" } });
+  fireEvent.change(message, { target: { value: "test message" } });
+  await act( async() => { fireEvent.click(button) });
+
+  expect(firstName.value).toBe("Anh");
+  expect(lastName.value).toBe("Truong");
+  expect(email.value).toBe("test@gmail.com");
+  expect(message.value).toBe("test message");
+
+
+});
